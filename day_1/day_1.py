6924a32b5a098f7bcbb95a5d8ddaebd8e1d06cff
Royce <royceang96@gmail.com>
2023-12-05 22:34:51 +0800

    added comments

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -4,6 +4,7 @@ start_time = time.time()
 
 DAY_1_INPUT = 'day_1_input.txt'
 global_input = []
+
 def text_parser():
     file = open(DAY_1_INPUT, 'r')
     lines = file.readlines()
@@ -16,21 +17,26 @@ def numerical_val_in_characters_parser(cur_i, cur_line):
     valid_starting_char = {'o', 't', 'f', 's', 'e', 'n'}
     if(cur_line[cur_i] not in valid_starting_char):
             return [cur_i, -1, -1]
+
     digit_map = {'one' : 1, 'two' : 2, 'three' : 3, 'four' : 4, 'five': 5, 'six' : 6, 'seven' : 7, 'eight' : 8, 'nine' : 9}
     forward_i = cur_i + 1
+
     while (forward_i < len(cur_line) and not cur_line[forward_i].isnumeric()):
         forward_i += 1
+
     tmp_line = cur_line[cur_i: forward_i]
     first_digit = [-1]
     second_digit = [-1]
+
     def handle_digit_priority(cur_digit):
         if first_digit[0] == -1:
             first_digit[0] = cur_digit
             return
         second_digit[0] = cur_digit
+
     i = 0
-    while i < len(tmp_line):
-        updated = False
+    while i < len(tmp_line): 
+        updated = False      
         for k, v in digit_map.items():
             if (i < len(tmp_line) and tmp_line[i] not in valid_starting_char):
                 break
@@ -47,6 +53,9 @@ def numerical_val_in_characters_parser(cur_i, cur_line):
         
 
 def value_parser_using_first_and_last_digits():
+    '''
+    O(N*M^2) -- where N refers to length of each line parsed; M^2 due to string comparison
+    '''
     line_no = 1
     res = 0 
     for line in global_input:
@@ -54,7 +63,7 @@ def value_parser_using_first_and_last_digits():
         first_digit = -1
         last_digit = -1
         i = 0
-        while i < len(line):
+        while i < len(line): # O(N)
             tmp_res = numerical_val_in_characters_parser(i, line)
             i, tmp_first_digit, tmp_second_digit = tmp_res[0], tmp_res[1], tmp_res[2]
             if first_digit == -1 and tmp_first_digit != -1:
